17f16ac80f5ca09018278ef55420de6ffaeb8f3d
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-08-15 22:38:07 +0530

    positional encoding class created

--- transformer/model.py
+++ transformer/model.py
@@ -14,4 +14,28 @@ class InputEmbedding(nn.Module):
         return self.embedding(x) * math.sqrt(self.d_model)
     
 
-    
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model:int, seq_len:int, dropout:float):
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout()
+
+
+        #create matrix of shape (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
+        # Create a vector of shape (seq_len, 1)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+
+        # Apply the sin to even positions and cos for odd position
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0) #(1, seq_len, d_model)
+        
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)
+        return self.dropout(x)
